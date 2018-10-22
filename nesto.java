74863ebdbb8f9035189a4ffd7046fd2fde5b6a4c
Šimo Matijević <simo.matijevic@gmail.com>
2018-10-22 18:24:58 +0200

    Delete nesto.java

--- nesto.java
+++
@@ -1,8 +0,0 @@
-public void dohvat(String upit){
-		try{
-			//String upit = "select * from automobil";
-			rs = stmt.executeQuery(upit);
-			while(rs.next()){
-				String tip = rs.getString("tip");
-				System.out.println(tip);
-			}
